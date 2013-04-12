170f33b13b97e8d5979d2728175865cb455c6f83
Leo Royzengurt <rebellabs@gmail.com>
2013-04-12 13:10:48 -0400

    first commit comrade

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wilr::Application.config.secret_token = 'f1af01d449b5e34a7447fdd5b2d80aa106258b7dd6d530d086f6a7c0964684bf046d6dd37c1e688c9160d8354a1b1bf66cfd14db4c84f585ff700c096c4869d6'
